8723f0e18f12f86bb8d0977757278f625694b7c1
Reid Prichard <reideprichard@gmail.com>
2024-02-19 22:07:01 -0500

    Finished window_manager basic functionality.

--- utils.c
+++ utils.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 #define BUFFER_LEN 256
+// TODO: Replace BUFFER_LEN with something more sensible
 
 int forceSetForegroundWindow(HWND window) {
   // Tricks here courtesy of https://gist.github.com/Aetopia/1581b40f00cc0cadc93a0e8ccb65dc8c
@@ -33,7 +34,7 @@ int activateWindowByTitle(const TCHAR* windowTitle) {
   }
 }
 
-int getForegroundWindowInfo(HWND* foregroundWindow, TCHAR** processName, TCHAR** windowTitle) {
+int getForegroundWindowInfo(HWND* foregroundWindow, TCHAR* processName, TCHAR* windowTitle) {
   DWORD dwProcId = 0;
   int returnCode = 0;
 
@@ -55,14 +56,15 @@ int getForegroundWindowInfo(HWND* foregroundWindow, TCHAR** processName, TCHAR**
   TCHAR* procEnd = strstr(processPath, ".exe");
   int end = strlen(procStart);
   if (strstr(processPath, ".exe")) {
-    end -= 4;
+    end -= 4; // Subtract 4 for the ".exe"
   }
+  end = max(0, min(end, BUFFER_LEN-1)); // Subtract one so there's room for the null byte
 
-  for (int charIndex = 0; charIndex < min(end, BUFFER_LEN-1); ++ charIndex) {
-    (*processName)[charIndex] = *(procStart + charIndex);
+  for (int charIndex = 0; charIndex < end; ++ charIndex) {
+    processName[charIndex] = *(procStart + charIndex); // sscanf_s would probably make this way easier?
   }
-  (*processName)[max(0, min(strlen(procStart)-4, BUFFER_LEN))] = '\0'; // Subtract 4 for the ".exe"
+  processName[end] = '\0';
   // ** Get window title **
-  returnCode |= GetWindowTextA(*foregroundWindow, *windowTitle, BUFFER_LEN);
+  returnCode |= GetWindowTextA(*foregroundWindow, windowTitle, BUFFER_LEN);
   return returnCode;
 }
--- window_manager.c
+++ window_manager.c
@@ -1,37 +1,111 @@
+// gcc window_manager.c -o window_manager.exe -g
+
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <minwindef.h>
 #include <Psapi.h>
 #include <stdbool.h>
 #include <stdio.h>
-// #include <WinUser.h>
+#include <stdlib.h>
+#include "utils.c"
 
-#define MAX_SAVED_WINDOWS 32
+#define MAX_SAVED_WINDOWS 999
+#define MAX_SAVED_WINDOWS_DIGITS 3
+#define STRING_LEN 256
+#define HWND_LEN 17 // Add one to account for null byte
 
-HWND* savedWindowHandles[MAX_SAVED_WINDOWS];
-TCHAR* savedWindowTitles[MAX_SAVED_WINDOWS];
+int activateSavedWindow(int index, TCHAR* filePath) {
+  if (index < 0 || index >= MAX_SAVED_WINDOWS) {
+    return 1;
+  }
+  TCHAR windowTitle[STRING_LEN];
+  TCHAR indexStr[MAX_SAVED_WINDOWS_DIGITS];
+  TCHAR hWndStr[HWND_LEN];
+  printf("Activating window #%d\n", index);
+  sprintf(indexStr, "%d", index);
 
-int activateSavedWindow(int index) {
-
-  return 0;
+  int returnCode = 1;
+  DWORD iniReadResult = GetPrivateProfileString(indexStr, "hWnd", NULL, hWndStr, HWND_LEN, filePath);
+  if (iniReadResult > 0) {
+    HWND hWnd;
+    sscanf_s(hWndStr, "%p", &hWnd);
+    printf("Attempting to restore by handle '%s'.\n", hWndStr);
+    returnCode = activateWindowByHandle(hWnd);
+  }
+  if (returnCode != 0) {
+    iniReadResult = GetPrivateProfileString(indexStr, "title", NULL, windowTitle, STRING_LEN, filePath);
+    if (iniReadResult > 0) {
+      printf("Attempting to restore by title.\n");
+      returnCode = activateWindowByTitle(windowTitle);
+    }
+  } 
+  return returnCode;
 }
 
-int saveWindow(int index) {
-
-  return 0;
-}
-
-int writeSavedWindows(const TCHAR* filePath) {
-
-  return 0;
-}
-
-int readSavedWindows(const TCHAR* filePath) {
-
-  return 0;
+int saveWindow(int index, TCHAR* filePath) {
+  if (index < 0 || index > MAX_SAVED_WINDOWS-1) {
+    return 1;
+  }
+  printf("Saving current window to index #%d\n", index);
+  TCHAR processTitle[STRING_LEN];
+  TCHAR windowTitle[STRING_LEN];
+  HWND hWnd;
+  getForegroundWindowInfo(&hWnd, &processTitle[0], &windowTitle[0]);
+  printf("Saving hWnd %p, process %s to %s\n", hWnd, processTitle, filePath);
+  TCHAR indexStr[MAX_SAVED_WINDOWS_DIGITS];
+  TCHAR hWndStr[HWND_LEN];
+  sprintf(indexStr, "%d", index);
+  sprintf(hWndStr, "%p", hWnd);
+  int iResult = WritePrivateProfileString(indexStr, "hWnd", hWndStr, filePath);
+  iResult |= WritePrivateProfileString(indexStr, "title", windowTitle, filePath);
+  return iResult;
 }
 
 int main(int argc, TCHAR *argv[]) {
-  printf("Starting...\n");
+  TCHAR iniFilePath[MAX_PATH] = "./saved_windows.ini";
+  TCHAR iniFileAbsolutePath[MAX_PATH];
+
+  int saveIndex = -1;
+  int loadIndex = -1;
+  
+  for (int i=min(1,argc); i<argc; ++i) {
+    // printf("%d\t%s\n",i,argv[i]);
+    if (strchr(argv[i], '-') != argv[i]) {
+      printf("ERROR: Invalid syntax.\n");
+      return 1;
+    }
+    else if (strstr(argv[i], "--path=")==argv[i]) {
+      sscanf_s(argv[i], "--path=%s", iniFilePath);
+    }
+    else if (strstr(argv[i], "--get-current-window")==argv[i]) {
+      printf("Current window: %p\n", GetForegroundWindow());
+    }
+    else if (strstr(argv[i], "--activate-window=")==argv[i]) {
+      HWND windowHandle;
+      sscanf_s(argv[i], "--activate-window=%p", &windowHandle);
+      activateWindowByHandle(windowHandle);
+    }
+    else if (strstr(argv[i], "--load-window=")==argv[i]) {
+      sscanf_s(argv[i], "--load-window=%d", &loadIndex);
+    }
+    else if (strstr(argv[i], "--save-window=")==argv[i]) {
+      sscanf_s(argv[i], "--save-window=%d", &saveIndex);
+    }
+  }
+  int result = GetFullPathName(iniFilePath, MAX_PATH, iniFileAbsolutePath, NULL);
+  if (result != strlen(iniFileAbsolutePath)) {
+    printf("Invalid config file path: %s", iniFilePath);
+    return 1;
+  }
+
+  printf("Path: %s\n", iniFileAbsolutePath);
+  if (saveIndex >= 0) {
+    if (!saveWindow(saveIndex, iniFileAbsolutePath)) {
+      printf("Could not write to '%s'\n", iniFilePath);
+    }
+  }
+  if (loadIndex >= 0) {
+    activateSavedWindow(loadIndex, iniFileAbsolutePath);
+  }
   return 0;
 }

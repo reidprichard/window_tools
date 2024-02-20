334756ee285a67db6aacf800382e934bdf26bf4c
Reid Prichard <reideprichard@gmail.com>
2024-02-20 09:40:52 -0500

    Set to use a default layer if the active process isn't a layer name in Kanata

--- kanata_client.c
+++ kanata_client.c
@@ -6,6 +6,7 @@
 #include <Psapi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "utils.c"
 
 // Needs to be linked to this. With mingw64, it's as simple as adding -lWs2_32 to the compile command
@@ -41,21 +42,19 @@ int getLayerNames(const TCHAR* configPath) {
     // 2. The line continues past layerStartStr (add 1 to account for newline)
     if (pos == buf && strlen(buf) > strlen(layerStartStr) + 1) {
       int layerNameLength = strlen(buf) - strlen(layerStartStr) - 1;
-
       strcpy_s(layerNames[layerNum], MAX_LAYER_NAME_LENGTH, buf + strlen(layerStartStr));
-      // for (int charIndex = 0; charIndex < min(layerNameLength, MAX_LAYER_NAME_LENGTH); ++charIndex) {
-      //   layerNames[layerNum][charIndex] = buf[strlen(layerStartStr)+charIndex];
-      // }
+      // Set the proper character to the null byte to indicate the end of the string.
       layerNames[layerNum][min(layerNameLength, MAX_LAYER_NAME_LENGTH)] = '\0';
       ++layerNum;
     }
   }
   layerCount = layerNum;
+  // Zero out unused entries in layerNames
   while (layerNum < MAX_LAYERS) {
     layerNames[layerNum][0] = '\0';
     ++layerNum;
   }
-  printf("%d layers found\n", layerCount);
+  printf("%d layers found in %s.\n", layerCount, configPath);
   for (int i = 0; i < layerCount; ++i) {
     printf("Layer %d: '%s'\n", i, layerNames[i]);
   }
@@ -63,7 +62,18 @@ int getLayerNames(const TCHAR* configPath) {
   return 0;
 }
 
+int checkLayer(const TCHAR* layerName) {
+  for (int i=0; i<layerCount; ++i) {
+    if (strcmp(layerName, layerNames[i]) == 0) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
 int initTcp(const TCHAR* host, const TCHAR* port, SOCKET* ConnectSocket) {
+  printf("Connecting to %s:%s...\n", host, port);
+
   int iResult;
   WSADATA wsaData;
   iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
@@ -77,7 +87,7 @@ int initTcp(const TCHAR* host, const TCHAR* port, SOCKET* ConnectSocket) {
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_protocol = IPPROTO_TCP;
 
-  iResult = getaddrinfo("localhost", "1337", &hints, &result);
+  iResult = getaddrinfo(host, port, &hints, &result);
 
   if (iResult != 0) {
     printf("getaddrinfo failed: %d\n", iResult);
@@ -111,16 +121,17 @@ int initTcp(const TCHAR* host, const TCHAR* port, SOCKET* ConnectSocket) {
   }
 
   TCHAR buf[BUFFER_LEN];
+  buf[0] = '\0';
   iResult = recv(*ConnectSocket, buf, BUFFER_LEN, 0);
   if (iResult > 0) {
-    printf("Bytes received: %d\n", iResult);
+    printf("TCP connection successful.\n");
+    printf("'%s'\n",buf);
   } else if (iResult == 0) {
     printf("Connection closed\n");
   } else {
     printf("recv failed: %d\n", WSAGetLastError());
   }
 
-  printf("TCP connection successful.\n");
   return 0;
 }
 
@@ -136,23 +147,12 @@ int sendTCP(SOCKET sock, TCHAR* msg) {
     return 1;
   }
   return 0;
-
-  // do {
-  //   iResult = recv(sock, recvbuf, sizeof(recvbuf), 0);
-  //   if (iResult > 0) {
-  //     printf("Bytes received: %d\n", iResult);
-  //   } else if (iResult == 0) {
-  //     printf("Connection closed\n");
-  //   } else {
-  //     printf("recv failed: %d\n", WSAGetLastError());
-  //   }
-  // } while (iResult > 0);
 }
 
 #define LAYER_CHANGE_TEMPLATE "{\"ChangeLayer\":{\"new\":\"%s\"}}"
 
 
-void loop(const TCHAR* hostname, const TCHAR* port) {
+void loop(const TCHAR* hostname, const TCHAR* port, const TCHAR* baseLayer) {
   SOCKET kanataSocket;
   int iResult = initTcp(hostname, port, &kanataSocket);
   if (iResult == SOCKET_ERROR) {
@@ -166,14 +166,25 @@ void loop(const TCHAR* hostname, const TCHAR* port) {
   TCHAR prevProcName[maxProcNameLen];
   TCHAR prevWinTitle[maxWinTitleLen];
   TCHAR* buf = malloc(MAX_LAYER_NAME_LENGTH + strlen(LAYER_CHANGE_TEMPLATE));
+  const TCHAR* activeLayerName;
   while(TRUE) {
+    // If the title of the active window changes
     if (getForegroundWindowInfo(&fg, &procName[0], &winTitle[0]) && (strcmp(winTitle, prevWinTitle) != 0)) {
-      printf("hWnd: '%p',\tTitle: '%s',\tProcess: '%s'\n", fg, winTitle, procName);
-      sprintf_s(buf, MAX_LAYER_NAME_LENGTH + strlen(LAYER_CHANGE_TEMPLATE), LAYER_CHANGE_TEMPLATE, procName);
+      printf("New window activated: hWnd='%p',\tTitle='%s',\tProcess='%s'\n", fg, winTitle, procName);
+
+      // If the window process (minus .exe) is in the list of layer names,
+      // activate that layer, otherwise activate baseLayer
+      if (checkLayer(procName)) {
+        activeLayerName = procName;
+      }
+      else {
+        activeLayerName = baseLayer;
+      }
+      sprintf_s(buf, MAX_LAYER_NAME_LENGTH + strlen(LAYER_CHANGE_TEMPLATE), LAYER_CHANGE_TEMPLATE, activeLayerName);
       iResult = sendTCP(kanataSocket, buf);
       if (iResult) {
         printf("Socket error. Attempting to reconnect.\n");
-        initTcp("localhost", "1337", &kanataSocket);
+        initTcp(hostname, port, &kanataSocket);
       }
       strcpy_s(prevWinTitle, maxWinTitleLen, winTitle);
     }
@@ -183,18 +194,29 @@ void loop(const TCHAR* hostname, const TCHAR* port) {
 }
 
 int main(int argc, TCHAR *argv[]) {
+  // The hostname of the TCP server Kanata is running on. I can't imagine ever
+  // needing this to be anything but localhost.
   TCHAR hostname[BUFFER_LEN] = "localhost";
+  // The port of Kanata's TCP server. You may want to put Kanata on something
+  // more obscure than 80.
   TCHAR port[BUFFER_LEN] = "80";
+  // The name of the default/base layer in your Kanata config. If the active
+  // window's process isn't found in your list of layer names, this layer will
+  // be activated instead.
+  TCHAR defaultLayer[MAX_LAYER_NAME_LENGTH] = "default";
   for (int i=min(1,argc); i<argc; ++i) {
     if (strchr(argv[i], '-') != argv[i]) {
       printf("ERROR: Invalid syntax.\n");
       return 1;
     }
     else if (strstr(argv[i], "--hostname=")==argv[i]) {
-      sscanf_s(argv[i], "--path=%s", hostname);
+      sscanf_s(argv[i], "--hostname=%s", hostname);
     }
     else if (strstr(argv[i], "--port=")==argv[i]) {
-      sscanf_s(argv[i], "--path=%s", port);
+      sscanf_s(argv[i], "--port=%s", port);
+    }
+    else if (strstr(argv[i], "--default-layer=")==argv[i]) {
+      sscanf_s(argv[i], "--default-layer=%s", defaultLayer);
     }
     else {
       printf("Invalid argument.\n");
@@ -204,6 +226,6 @@ int main(int argc, TCHAR *argv[]) {
   }
   printf("Starting...\n");
   getLayerNames("../murphpad_kanata.kbd");
-  loop(hostname, port);
+  loop(hostname, port, defaultLayer);
   return 0;
 }

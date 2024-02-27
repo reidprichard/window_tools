f7ad11ad64844b6042fbda99a1475febec2773a8
Reid Prichard <reideprichard@gmail.com>
2024-02-27 11:21:09 -0500

    Added return after --help

--- src/window_manager.c
+++ src/window_manager.c
@@ -173,6 +173,7 @@ int main(int argc, TCHAR *argv[]) {
       return 1;
     } else if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0) {
       printf("Valid args: --version | -v, --get-current-window, --activate-window=<hwnd>, --load-window=<num>, --save-window=<num>, --show, --path=<ini-path>\n");
+      return 0;
     } else if (strcmp(argv[i], "--version") == 0 || strcmp(argv[i], "-v") == 0) {
       printf("kanata_helper_daemon version %d.%d.%d\n", window_tools_VERSION_MAJOR, window_tools_VERSION_MINOR,
              window_tools_VERSION_PATCH);
